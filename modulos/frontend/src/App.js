2f400d4a153db200413e9335bef81f6a92bece45
Raphael Barreto <tnt.raphael@gmail.com>
2020-04-15 23:38:38 -0300

    Cadastrando Projetos

--- modulos/frontend/src/App.js
+++ modulos/frontend/src/App.js
@@ -15,8 +15,17 @@ function App() {
     })
   }, []);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Raphael Barreto"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
