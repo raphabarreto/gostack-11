356bba10c762fc22e35bb63e64dfbca90e9b1396
Raphael Barreto <tnt.raphael@gmail.com>
2020-05-31 17:53:55 -0300

    Relacionamento nos models

--- iniciando-back-end/src/database/migrations/1590878805425-CreateAppointments.ts
+++ iniciando-back-end/src/database/migrations/1590878805425-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1590878805425
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
