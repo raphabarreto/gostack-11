63d147edbed09513946b9f3079d2be806cf2c8d9
Raphael Barreto <tnt.raphael@gmail.com>
2020-05-30 22:38:33 -0300

    Repositório do TypeORM

--- iniciando-back-end/src/database/migrations/1590878805425-CreateAppointments.ts
+++ iniciando-back-end/src/database/migrations/1590878805425-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1590878805425
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
